54388407e2bb476716eed5009e5baac4dd3a90e8
JaydenPears <vladimir120905@gmail.com>
2023-09-07 18:35:56 +0300

    Start project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import libs:
+import React from 'react';
+
+// import static:
+// import './static/css/base.css';
+
+const App = () => {
+    return (
+        <div сlassName="AppDiv">
+            
+        </div>
+    )
+};
+
+export default App;

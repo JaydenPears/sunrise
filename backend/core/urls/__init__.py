438189360fcd4ea81da6a546230fb63ed542a829
Никита Лакин <nikita.lakin.topka@gmail.com>
2023-09-09 18:41:14 +0300

    SUN-4. Admin fixes

--- backend/core/urls/__init__.py
+++ backend/core/urls/__init__.py
@@ -1,10 +1,8 @@
 from django.urls import path, include
-from .admin import urlpatterns as admin_urlpatterns
 from .common import urlpatterns as common_urlpatterns
 
 app_name = 'core'
 
 urlpatterns = [
-    path('/admin/', include(admin_urlpatterns)),
-    path('/common/', include(common_urlpatterns)),
+    path('', include(common_urlpatterns)),
 ]
